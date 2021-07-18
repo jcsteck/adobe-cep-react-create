5e3d695563b1df75e05a1e15b8fc0d8e9aff9715
jcsteck <jcsteck@gmail.com>
2021-07-18 15:24:13 -0400

    Update pluginrc.js

--- pluginrc.js
+++ pluginrc.js
@@ -4,11 +4,11 @@ const srcFolder = path.join(root, "src")
 const destFolder = path.join(root, "dist")
 const certPath = path.join(destFolder, "cert.p12")
 module.exports = {
-    extensionBundleId: 'com.hendrix.demo',
-    extensionBundleName: 'demo',
+    extensionBundleId: 'com.oneSteck.demo',
+    extensionBundleName: 'Juans React Extension',
     extensionBundleVersion: '1.0.1',
     cepVersion: '7.0',
-    panelName: 'hendrix demo',
+    panelName: 'Juans React Ext demo',
     width: '400',
     height: '600',
     root: root,
@@ -21,7 +21,7 @@ module.exports = {
         },
         selfSign: {
             country: 'US',
-            province: 'CA',
+            province: 'VA',
             org: 'org',
             name: 'name',
             password: 'password',
